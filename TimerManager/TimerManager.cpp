157a54fd62a7a998084090856f8a9b66e97c7933
Javier Presmanes <presmanes3@gmail.com>
2020-01-06 14:06:48 +0100

    Created TimerManager

---
+++ TimerManager/TimerManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by presmanes3 on 6/1/20.
+//
+
+#include "TimerManager.h"
---
+++ TimerManager/TimerManager.h
@@ -0,0 +1,18 @@
+//
+// Created by presmanes3 on 6/1/20.
+//
+
+#ifndef TIMERMANAGERPROJECT_TIMERMANAGER_H
+#define TIMERMANAGERPROJECT_TIMERMANAGER_H
+
+
+#include <cstdint>
+
+class TimerManager {
+
+public:
+    uint8_t l = 0;
+};
+
+
+#endif //TIMERMANAGERPROJECT_TIMERMANAGER_H
